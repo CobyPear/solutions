1dcf73100824cec07069c4987dfa19052420ad4e
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-09-16 10:59:37 -0500

    leetcode baseball game problem

---
+++ TechnicalInterviewPractice/baseballGame.js
@@ -0,0 +1,97 @@
+/*
+You're now a baseball game point recorder.
+
+Given a list of strings, each string can be one of the 4 following types:
+
+Integer (one round's score): Directly represents the number of points you get in this round.
+"+" (one round's score): Represents that the points you get in this round are the sum of the last two valid round's points.
+"D" (one round's score): Represents that the points you get in this round are the doubled data of the last valid round's points.
+"C" (an operation, which isn't a round's score): Represents the last valid round's points you get were invalid and should be removed.
+Each round's operation is permanent and could have an impact on the round before and the round after.
+
+You need to return the sum of the points you could get in all the rounds.
+
+Example 1:
+Input: ["5","2","C","D","+"]
+Output: 30
+Explanation: 
+Round 1: You could get 5 points. The sum is: 5.
+Round 2: You could get 2 points. The sum is: 7.
+Operation 1: The round 2's data was invalid. The sum is: 5.  
+Round 3: You could get 10 points (the round 2's data has been removed). The sum is: 15.
+Round 4: You could get 5 + 10 = 15 points. The sum is: 30.
+Example 2:
+Input: ["5","-2","4","C","D","9","+","+"]
+Output: 27
+Explanation: 
+Round 1: You could get 5 points. The sum is: 5.
+Round 2: You could get -2 points. The sum is: 3.
+Round 3: You could get 4 points. The sum is: 7.
+Operation 1: The round 3's data is invalid. The sum is: 3.  
+Round 4: You could get -4 points (the round 3's data has been removed). The sum is: -1.
+Round 5: You could get 9 points. The sum is: 8.
+Round 6: You could get -4 + 9 = 5 points. The sum is 13.
+Round 7: You could get 9 + 5 = 14 points. The sum is 27.
+Note:
+The size of the input list will be between 1 and 1000.
+Every integer represented in the list will be between -30000 and 30000.
+
+Algorithm
+iterate around the dataset
+
+
+*/
+
+const calPoints = ops => {
+
+    const pointTracker = [];
+    let output = 0;
+
+    for (let i = 0; i < ops.length; i++) {
+        const currentString = ops[i]
+        // if the string is a number, 
+        if (!isNaN(currentString)) {
+            const num = parseInt(currentString)
+            // push the string to pointTracker array
+            pointTracker.push(num);
+            // add th is number to output
+            output += num;
+        }
+        // else if ops[i] === "+"
+        else if (currentString === "+") {
+            // reference last 2 points in pointTracker array
+            const last = pointTracker[pointTracker.length-1];
+            const secondToLast = pointTracker[pointTracker.length-2];
+            // push this number into point tracker array
+            const sum = last + secondToLast;
+            pointTracker.push(sum);
+            // add this number to the output
+            output += sum;
+        }
+        // else if ops[i] === 'D'
+        else if (currentString === "D") {
+            // refernce the last point in pointTracker array 
+            const last = pointTracker[pointTracker.length-1];
+            // double this number and add it to the output and pointTracker
+            const double = last*2;
+            pointTracker.push(double);
+            output += double;
+
+        }
+
+        // else ops[i] === 'C'
+        else {
+            // remove the last point in the pointTracker array (.pop())
+            const removedPoint = pointTracker.pop();
+            // subtract that value from output
+            output -= removedPoint;
+
+        }
+
+    }
+
+    return output;
+}
+
+console.log(calPoints(["5","2","C","D","+"]))
+console.log(calPoints(["5","-2","4","C","D","9","+","+"]))
